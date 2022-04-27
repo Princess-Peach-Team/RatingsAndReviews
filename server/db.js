0fad748580e687b6427df083c02c4896f8772592
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-04-26 21:31:44 -0600

    All routes and database queries are now completed and working. Db queries have been optimized to be as quick as 1-1.5ms per query.

--- server/db.js
+++ server/db.js
@@ -7,24 +7,7 @@ const pool = new Pool({
   database: 'ratingsandreviews'
 });
 
-/* This works v. Use as reference for building functions */
-// let query = pool.query(`SELECT * FROM review WHERE product`)
-// pool.connect()
-  // .then(() => console.log('Successfully connected to PostgreSQL: ratingsandreviews'))
-  // .then(() => pool.query('SELECT * FROM reviews_photos;'))
-  // .then((res) => {
-  //   console.table(res.rows[0]);
-  //   // console.log(res.rows[0]);
-  //   // let obj = res.rows[0];
-  //   // for (const prop in obj) {
-  //   //   console.log('prop -> ', prop);
-  //   //   console.log('val -> ', obj[prop]);
-  //   // }
-  // })
-  // .catch((err) => console.error(err));
-
 const GETProductReviews = (productId) => {
-  // return pool.query(`SELECT * FROM review JOIN photos ON review.id = photos.review_id WHERE product_id = ${productId};`); //WHERE product_id = ${productId}
   return pool.query(`SELECT
     review.id AS id,
     review.product_id AS product_id,
@@ -35,7 +18,7 @@ const GETProductReviews = (productId) => {
     review.recommend AS recommend,
     review.reported AS reported,
     review.reviewer_name AS reviewer_name,
-    review.response AS reviewer_response,
+    review.response AS response,
     review.helpfulness AS helpfulness,
     (array_agg(
       json_build_object(
@@ -50,17 +33,169 @@ const GETProductReviews = (productId) => {
 };
 
 const GETMeta = (productId) => {
-  return pool.query(`SELECT * FROM characteristics WHERE product_id = ${productId}`);
+  return pool.query(`SELECT
+    review.product_id AS product_id,
+    json_build_object(
+      '1', (SELECT COUNT(rating) FROM review WHERE product_id = $1
+      AND rating = 1),
+      '2', (SELECT COUNT(rating) FROM review WHERE product_id = $1
+      AND rating = 2),
+      '3', (SELECT COUNT(rating) FROM review WHERE product_id = $1
+      AND rating = 3),
+      '4', (SELECT COUNT(rating) FROM review WHERE product_id = $1
+      AND rating = 4),
+      '5', (SELECT COUNT(rating) FROM review WHERE product_id = $1
+      AND rating = 5)
+    ) AS rating,
+    json_build_object(
+      '0', (SELECT COUNT(recommend) FROM review WHERE recommend = 'false' AND product_id = $1),
+      '1', (SELECT COUNT(recommend) FROM review WHERE recommend = 'true' AND product_id = $1)
+    ) AS recommend,
+    json_object_agg(
+      characteristics.name, json_build_object(
+        'id', characteristics.id,
+        'value', (SELECT avg(meta.value) FROM meta WHERE meta.characteristic_id = characteristics.id)
+      )
+    ) AS characteristics
+    FROM review
+    LEFT JOIN characteristics ON characteristics.product_id = review.product_id
+    LEFT JOIN meta ON meta.characteristic_id = characteristics.id
+    WHERE review.product_id = $1
+    GROUP BY review.product_id`, [productId]);
 };
 
-const PUTHelpful = () => {};
+const PUTHelpful = (reviewId) => {
+  let query = {
+    text: 'UPDATE review SET helpfulness = helpfulness + 1 WHERE id = $1;',
+    values: [reviewId]
+  };
+  return pool.query(query);
+};
 
-const PUTReport = () => {};
+const PUTReport = (reviewId) => {
+  let query = {
+    text: `UPDATE review SET reported = 'true' WHERE id = $1;`,
+    values: [reviewId]
+  }
+  return pool.query(query);
+};
 
-const POSTReviews = () => {};
+const POSTReviews = (productId, rating, summary, body, recommend, name, email, photos, chars) => {
+  let date = Date.now();
+  let query = {
+    text: 'EXPLAIN ANALYZE INSERT INTO review (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id;',
+    values: [productId, rating, date, summary, body, recommend, 'false', name, email, 'null']
+  }
+  pool.query(query)
+    .then(({ rows }) => {
+      console.log('first rows -> ', rows);
+      const id = rows[0].id;
+      let photosVals = [];
+      let photosQueryString = 'EXPLAIN ANALYZE INSERT INTO reviews_photos (review_id, url) VALUES '
+      for (let i = 1; i < photos.length + 1; i++) {
+        photosVals.push(id);
+        photosVals.push(photos[i]);
+        if (i === photos.length) {
+          photosQueryString += `($${i}, $${i + 1});`;
+        } else {
+          photosQueryString += `($${i}, ${i + 1}), `;
+        }
+      }
+      let photosQuery = {
+        text: photosQueryString,
+        values: photosVals
+      }
+      pool.query(photosQuery);
+
+      let charsQueryString = 'EXPLAIN ANALYZE INSERT INTO characteristics (product_id, name) VALUES ';
+      let charsVals = [];
+      let charsReviewsQueryString = 'EXPLAIN ANALYZE INSERT INTO meta (characteristic_id, review_id, value) VALUES ';
+
+      const charsLength = Object.keys(chars).length;
+      let count = 0;
+      let nums = 0;
+      let nums2 = 0;
+      for (const char in chars) {
+        count += 1;
+        nums += 1;
+        nums2 += 1;
+        charsVals.push(productId, char);
+        if (count === charsLength) {
+          charsQueryString += `($${nums}, $${nums + 1}) RETURNING id;`;
+          charsReviewsQueryString += `($${nums2}, $${nums2 + 1}, $${nums2 + 2});`;
+          nums += 1;
+          nums2 += 2;
+        } else {
+          charsQueryString += `($${nums}, $${nums + 1}), `;
+          charsReviewsQueryString += `($${nums2}, $${nums2+ 1}, $${nums2+ 2}), `;
+          nums += 1;
+          nums2 += 2;
+        }
+      }
+
+      let charsQuery = {
+        text: charsQueryString,
+        values: charsVals
+      };
+      pool.query(charsQuery)
+        .then(({ rows }) => {
+          console.log('second rows -> ', rows);
+          let charsReviewsVals = [];
+          let count = 0;
+          for (const char in chars) {
+            charsReviewsVals.push(rows[count].id, id, chars[char]);
+            count += 1;
+          }
+          let charsReviewsQuery = {
+            text: charsReviewsQueryString,
+            values: charsReviewsVals
+          };
+          pool.query(charsReviewsQuery)
+            .then(({ rows }) => console.log('third rows -> ', rows))
+            .catch((err) => console.error(err));
+        })
+        .catch((err) => {
+          console.error(err)
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
 module.exports.getProduct = GETProductReviews;
 module.exports.getMeta = GETMeta;
 module.exports.putHelpful = PUTHelpful;
 module.exports.putReport = PUTReport;
 module.exports.postReviews = POSTReviews;
+
+
+// SELECT
+//     review.product_id AS product_id,
+//     json_build_object(
+//       '1', (SELECT COUNT(rating) FROM review WHERE product_id = 1
+//       AND rating = 1),
+//       '2', (SELECT COUNT(rating) FROM review WHERE product_id = 1
+//       AND rating = 2),
+//       '3', (SELECT COUNT(rating) FROM review WHERE product_id = 1
+//       AND rating = 3),
+//       '4', (SELECT COUNT(rating) FROM review WHERE product_id = 1
+//       AND rating = 4),
+//       '5', (SELECT COUNT(rating) FROM review WHERE product_id = 1
+//       AND rating = 5)
+//     ) AS rating,
+//     json_build_object(
+//       '0', (SELECT COUNT(recommend) FROM review WHERE recommend = 'false' AND product_id = 1),
+//       '1', (SELECT COUNT(recommend) FROM review WHERE recommend = 'true' AND product_id = 1)
+//     ) AS recommend,
+//     json_object_agg(
+//       characteristics.name, json_build_object(
+//         'id', characteristics.id,
+//         'value', (SELECT avg(meta.value) FROM meta WHERE meta.characteristic_id = characteristics.id)
+//       )
+//     ) AS characteristics
+//     FROM review
+//     LEFT JOIN characteristics ON characteristics.product_id = review.product_id
+//     LEFT JOIN meta ON meta.characteristic_id = characteristics.id
+//     WHERE review.product_id = 1
+//     GROUP BY review.product_id
--- server/routes.js
+++ server/routes.js
@@ -8,8 +8,30 @@ const router = express.Router();
 router.get('/reviews/:product_id', (req, res) => {
   getProduct(req.params.product_id)
     .then(({ rows }) => {
-      console.log(rows)
-      res.send(rows);
+      console.log('rows ', rows);
+      for (let i = 0; i < rows.length; i++) {
+        if (rows[i].recommend === 'true') {
+          rows[i].recommend = true;
+        } else {
+          rows[i].recommend = false;
+        }
+        if (rows[i].reported === 'true') {
+          rows[i].reported = true;
+        } else {
+          rows[i].reported = false;
+        }
+        if (rows[i].response === 'null') {
+          rows[i].response = null;
+        }
+      }
+      console.log('rows after: ', rows);
+      let returnObj = {
+        product: req.params.product_id,
+        page: Number(req.query.page) || 1,
+        count: Number(req.query.count) || 5,
+        results: rows
+      };
+      res.send(returnObj);
     })
     .catch((err) => console.error(err));
 });
@@ -17,26 +39,35 @@ router.get('/reviews/:product_id', (req, res) => {
 // route to get reviews metadata by product id
 router.get('/reviews/meta/:product_id', (req, res) => {
   getMeta(req.params.product_id)
-    .then(({ rows }) => {
-      console.log('meta results: ', rows);
-      res.send(rows);
+    .then((data) => {
+      res.send(data.rows);
     })
     .catch((err) => console.error(err));
 });
 
 // route to mark a review as helpful
 router.put('/reviews/:review_id/helpful', (req, res) => {
-  // TODO: implement function calls from db.js
+  putHelpful(req.params.review_id)
+    .then((rows) => {
+      res.send(rows);
+    })
+    .catch((err) => console.error(err));
 });
 
 // route to report a review
 router.put('/reviews/:review_id/report', (req, res) => {
-  // TODO: implement function calls from db.js
+  putReport(req.params.review_id)
+    .then((rows) => {
+      res.send(rows);
+    })
+    .catch((err) => console.error(err));
 });
 
 // route to post a review
 router.post('/reviews', (req, res) => {
-  // TODO: implement function calls from db.js
+  let { body } = req;
+  postReviews(body.product_id, body.rating, body.summary, body.body, body.recommend, body.name, body.email, body.photos, body.characteristics);
+  res.send('Thank you for your review!');
 });
 
 module.exports = router;
